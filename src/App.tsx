a53b340431fd9cdc56a4b2a471767b5499fb7795
Shamsur Raza Chowdhury <shamsurraza.chowdhury@tuni.fi>
2022-01-29 18:09:49 +0200

    prior to calc implemention

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import InputForm from "./components/InputForm";
+
+export interface IState {
+  data: {
+    cartValue: number;
+    deliveryDistance: number;
+    amountOfItems: number;
+    dateAndTime: Date;
+  };
+  setValue: React.Dispatch<
+    React.SetStateAction<IState["data"]>>;
+}
 
 function App() {
+  const [value, setValue] = useState<IState["data"]>({
+    cartValue: 0,
+    deliveryDistance: 0,
+    amountOfItems: 0,
+    dateAndTime: new Date(),
+  });
+
   return (
     <div>
-      
+      <h1> Wolt delivery </h1>
+      <InputForm data={value} setValue={setValue}/>
     </div>
   );
 }
